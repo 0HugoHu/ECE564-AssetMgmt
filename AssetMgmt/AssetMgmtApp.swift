ffeeb4add4c23b95364ea5aa6f669b1c986c0a39
yh342 <yh342@duke.edu>
2023-10-22 20:51:32 -0400

    Initial Commit

---
+++ AssetMgmt/AssetMgmtApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssetMgmtApp.swift
+//  AssetMgmt
+//
+//  Created by Hugooooo on 10/22/23.
+//
+
+import SwiftUI
+
+@main
+struct AssetMgmtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
