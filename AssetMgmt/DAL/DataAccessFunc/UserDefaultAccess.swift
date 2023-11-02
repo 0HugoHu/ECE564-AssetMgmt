16ce9313ca5f2f7178cc9c632253a99378754b83
Janus Chen <janus.chen@duke.edu>
2023-11-01 20:46:07 -0400

    Feat: saveUserInfo() and getUserInfo()

---
+++ AssetMgmt/DAL/DataAccessFunc/UserDefaultAccess.swift
@@ -0,0 +1,29 @@
+//
+//  UserDefaultAccess.swift
+//  AssetMgmt
+//
+//  Created by Athenaost on 11/1/23.
+//
+
+import Foundation
+
+func saveUserInfo(_ userInfo: UserInfoAPIResponse) {
+    do {
+        let data = try JSONEncoder().encode(userInfo)
+        UserDefaults.standard.set(data, forKey: "userInfo")
+    } catch {
+        // log.error(error)
+    }
+}
+
+func getUserInfo() -> UserInfoAPIResponse? {
+    if let data = UserDefaults.standard.data(forKey: "userInfo") {
+        do {
+            let userInfo = try JSONDecoder().decode(UserInfoAPIResponse.self, from: data)
+            return userInfo
+        } catch {
+            // log.error(error)
+        }
+    }
+    return nil
+}
